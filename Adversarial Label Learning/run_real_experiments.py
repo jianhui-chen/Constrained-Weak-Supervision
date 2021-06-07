0896677854c62bf7ef5b8f04f9bc8d7aa7c21f52
pgavazzi1 <patrick.gavazzi@tufts.edu>
2021-06-07 17:07:42 -0400

    Cleaned up
    Cleaned up function calls a little and moved some files around

--- Adversarial Label Learning/run_real_experiments.py
+++ Adversarial Label Learning/run_real_experiments.py
@@ -4,7 +4,7 @@ from ge_criterion_baseline import *
 from utilities import saveToFile, runBaselineTests, getModelAccuracy, getWeakSignalAccuracy
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-from experiments import default_reader, obs_network_reader
+import default_reader
 
 
 def train_weak_signals(data, weak_signal_data, num_weak_signal):
@@ -317,16 +317,23 @@ def run_tests():
     """
  
     # # # # # # # # # # # #
-    # normal experiments  #
+    # breast cancer       #
     # # # # # # # # # # # #
 
-    # #for breast cancer classification dataset, select the mean radius, radius se and worst radius as weak signals
+    #for breast cancer classification dataset, select the mean radius, radius se and worst radius as weak signals
     print("Running breast cancer experiment...")
     views                  = {0:0, 1:10, 2:20}
     datapath               = 'datasets/breast-cancer/wdbc.data'
     savepath               = 'results/json/breast_cancer.json'
     load_and_process_data  = default_reader.breast_cancer_load_and_process_data
     default_reader.run_experiment(run_experiment, saveToFile, views, datapath, load_and_process_data, savepath)
+
+
+
+
+    # # # # # # # # # # # #
+    # obs network         #
+    # # # # # # # # # # # #
 
     # #for obs network dataset, select the Utilized Bandwidth Rate, Packet drop rate and Flood Status as weak signals
     print("Running obs network experiment...")
@@ -335,6 +342,12 @@ def run_tests():
     savepath               = 'results/json/obs_network.json'
     load_and_process_data  = default_reader.obs_load_and_process_data
     default_reader.run_experiment(run_experiment, saveToFile, views, datapath, load_and_process_data, savepath)
+
+
+
+    # # # # # # # # # # # #
+    # cardio              #
+    # # # # # # # # # # # #
  
     # #Use AC, MLTV and Median as weak signal views
     print("Running cardio experiment...")
@@ -345,51 +358,61 @@ def run_tests():
     default_reader.run_experiment(run_experiment, saveToFile, views, datapath, load_and_process_data, savepath)
 
 
-
+def run_bounds_experiment():
+    """
+    Runs experiment.
+    :return: None
+    """
 
     # # # # # # # # # # # #
-    # bounds experiments  #
+    # breast cancer       #
+    # # # # # # # # # # # #
+    
+    # for breast cancer classification dataset, select the mean radius, radius se and worst radius as weak signals
+    views                     = {0:0, 1:10, 2:20}
+    path                      = 'results/json/bc_bounds.json'
+    load_and_process_data     = default_reader.breast_cancer_load_and_process_data
+    data_and_weak_signal_data = default_reader.create_weak_signal_view('datasets/breast-cancer/wdbc.data', views, load_and_process_data)
+    default_reader.run_bounds_experiment(bound_experiment, data_and_weak_signal_data, path) 
+
+    # # # # # # # # # # # #
+    # obs network         #
     # # # # # # # # # # # #
 
-    # # un-comment to run bounds experimrnt in the paper
-    
-    #for breast cancer classification dataset, select the mean radius, radius se and worst radius as weak signals
-    # views                     = {0:0, 1:10, 2:20}
-    # path                      = 'results/json/bc_bounds.json'
-    # load_and_process_data     = default_reader.breast_cancer_load_and_process_data
-    #data_and_weak_signal_data = default_reader.create_weak_signal_view('datasets/breast-cancer/wdbc.data', views, load_and_process_data)
-    #default_reader.run_bounds_experiment(bound_experiment, data_and_weak_signal_data, path) 
-
-    
-    #for obs network dataset, select the Utilized Bandwidth Rate, Packet drop rate and Flood Status as weak signals
-    # views                     = {0:1, 1:2, 2:20}
-    # path                      = 'results/json/obs_bounds.json'
-    # load_and_process_data     = default_reader.obs_load_and_process_data
-    # data_and_weak_signal_data = default_reader.create_weak_signal_view('datasets/breast-cancer/wdbc.data', views, load_and_process_data)
-    # default_reader.run_bounds_experiment(bound_experiment, data_and_weak_signal_data, path)
-    # obs_network_reader.run_bounds_experiment(bound_experiment)
+    # for obs network dataset, select the Utilized Bandwidth Rate, Packet drop rate and Flood Status as weak signals
+    views                     = {0:1, 1:2, 2:20}
+    path                      = 'results/json/obs_bounds.json'
+    load_and_process_data     = default_reader.obs_load_and_process_data
+    data_and_weak_signal_data = default_reader.create_weak_signal_view('datasets/breast-cancer/wdbc.data', views, load_and_process_data)
+    default_reader.run_bounds_experiment(bound_experiment, data_and_weak_signal_data, path)
+    obs_network_reader.run_bounds_experiment(bound_experiment)
 
 
 
 
+def run_dep_err_experiment():
 
-    # # # # # # # # # # # # # # # # #
-    # dependency error experiments  #
-    # # # # # # # # # # # # # # # # # 
+    # # # # # # # # # # # #
+    # cardio              #
+    # # # # # # # # # # # #
 
-    # # # un-comment to run dependency error experiment in the paper
-
-    # #print("Running dependent error on cardio experiment...")
-    # #Use AC, MLTV and Median as weak signal views
-    # views = {0:1, 1:18}
-    # # repeat the bad weak signal 
-    # for i in range(2,10):
-    #     views[i] = 18
-    # path                      = 'results/json/cardio_error.json'
-    # load_and_process_data     = default_reader.cardio_load_and_process_data
-    # data_and_weak_signal_data = default_reader.create_weak_signal_view('datasets/cardiotocography/cardio.csv', views, load_and_process_data)
-    # #default_reader.run_dep_error_exp(dependent_error_exp, data_and_weak_signal_data, path)
-
+    print("Running dependent error on cardio experiment...")
+    #Use AC, MLTV and Median as weak signal views
+    views = {0:1, 1:18}
+    # repeat the bad weak signal 
+    for i in range(2,10):
+        views[i] = 18
+    path                      = 'results/json/cardio_error.json'
+    load_and_process_data     = default_reader.cardio_load_and_process_data
+    data_and_weak_signal_data = default_reader.create_weak_signal_view('datasets/cardiotocography/cardio.csv', views, load_and_process_data)
+    #default_reader.run_dep_error_exp(dependent_error_exp, data_and_weak_signal_data, path)
 
 if __name__ == '__main__':
     run_tests()
+
+    # # un-comment to run bounds experimrnt in the paper
+    # run_bounds_experiment()
+
+    # # un-comment to run dependency error experiment in the paper
+    #run_dep_err_experiment()
+
