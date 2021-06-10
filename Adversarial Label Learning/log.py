45caf118363bfa5b8231d818df7c28ae6f8513da
Elise Cho <elise.cho@tufts.edu>
2021-06-10 11:14:38 -0400

    Copied over TB log code

--- Adversarial Label Learning/log.py
+++ Adversarial Label Learning/log.py
@@ -1,1 +1,30 @@
-# file for logging calculations
+""" 
+file for logging calculations
+    Currently focusing on Tensorboard logs, with add for normal logs
+Borrowing code from Jeasine Ma
+"""
+
+import tensorflow as tf
+import numpy as np
+
+
+class Logger(object):
+    """Logging in tensorboard without tensorflow ops."""
+
+    def __init__(self, log_dir):
+        """Creates a summary writer logging to log_dir."""
+        self.writer = tf.summary.FileWriter(log_dir)
+
+    def log_scalar(self, tag, value, step):
+        """Log a scalar variable.
+        Parameter
+        ----------
+        tag : basestring
+            Name of the scalar
+        value
+        step : int
+            training iteration
+        """
+        summary = tf.Summary(value=[tf.Summary.Value(tag=tag,
+                                                     simple_value=value)])
+        self.writer.add_summary(summary, step)
