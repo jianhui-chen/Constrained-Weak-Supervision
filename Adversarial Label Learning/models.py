51fd978fc1fa1d905c20a9004c278f5a87f7564b
Elise Cho <elise.cho@tufts.edu>
2021-06-18 22:13:56 -0400

    Added more framework

--- Adversarial Label Learning/models.py
+++ Adversarial Label Learning/models.py
@@ -7,10 +7,51 @@ class ALL():
     """
     Adversarial Label Learning Classifier
 
+    This class implements ALL training on a set of data
+    Comments to be modified
+
     """
 
-    def __init__(self, logging=False):
-        self.logging = logging      # might want to rename this to verbose
+    def __init__(self, max_iter=10000, logging=False):
+        self.max_iter = max_iter
+        self.logging = logging      # might want to rename this 
+
+    
+    def fit(self, X):
+        """
+        Fits the model according to given training data (X)
+
+        Parameters
+        ----------
+        X : {}
+
+
+        Returns
+        -------
+        model
+
+        """
+        print(X) #placeholder code
+
+        # Would need to do if statement with self.logging to implement logger
+
+
+    def predict_proba(self, X):     # Note to self: this should replace "probablity" function in train_classifier
+        """
+        Computes probability estimates for given class
+        Should be able to be extendable for multi-class implementation
+
+        Parameters
+        ----------
+        X : 
+
+
+        Returns
+        -------
+        label predictions
+
+        """
+        print(X) # placeholder code
         
 
 class Baseline():
