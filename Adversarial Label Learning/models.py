9d9e418dabf442dddd57b07c0f9619f6c6bac773
Elise Cho <elise.cho@tufts.edu>
2021-06-18 21:51:09 -0400

    Added rougher outline for class structure

--- Adversarial Label Learning/models.py
+++ Adversarial Label Learning/models.py
@@ -1,1 +1,23 @@
 # Note for ALL model class –– inherit from sklean base.py?
+
+# Need to consider if we want to create an abstract base class
+
+
+class ALL():
+    """
+    Adversarial Label Learning Classifier
+
+    """
+
+    def __init__(self, logging=False):
+        self.logging = logging      # might want to rename this to verbose
+        
+
+class Baseline():
+    """
+    Baseline Classifier
+    Need to add more on its functionality. 
+    """
+
+    def __init__(self, logging=False):
+        self.logging = logging
