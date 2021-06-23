ba597bf25fe42306e091727c9722cfb487325d6a
pgavazzi1 <patrick.gavazzi@tufts.edu>
2021-06-23 11:05:36 -0400

    added example from CLL code

--- Adversarial Label Learning/weak_signals.py
+++ Adversarial Label Learning/weak_signals.py
@@ -105,3 +105,22 @@ def get_w_data_dicts(data_obj, min_weak_signals, total_weak_signals):
 
 
     return w_data_dicts
+
+
+def read_text_data(datapath):
+    """ Read text datasets """
+
+    train_data = np.load(datapath + 'data_features.npy', allow_pickle=True)[()]
+    weak_signals = np.load(datapath + 'weak_signals.npy', allow_pickle=True)[()]
+    train_labels = np.load(datapath + 'data_labels.npy', allow_pickle=True)[()]
+    test_data = np.load(datapath +'test_features.npy', allow_pickle=True)[()]
+    test_labels = np.load(datapath + 'test_labels.npy', allow_pickle=True)[()]
+
+    if len(weak_signals.shape) == 2:
+        weak_signals = np.expand_dims(weak_signals.T, axis=-1)
+
+    data = {}
+    data['train'] = train_data, train_labels
+    data['test'] = test_data, test_labels
+    data['weak_signals'] = weak_signals
+    return data
